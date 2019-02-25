cfad711379c82aa3c9693058aa20211b978101b2
Dede Gunawan <dede@unsil.ac.id>
2019-02-25 19:02:47 +0700

    Initial Commit

---
+++ resources/views/detail-produk.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tik_squad
+ * Date: 2/25/19
+ * Time: 6:41 PM
+ */
---
+++ resources/views/keranjang.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tik_squad
+ * Date: 2/25/19
+ * Time: 6:42 PM
+ */
